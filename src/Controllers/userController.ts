7c8222dd93570c5d8054a7e99808a9ae888cf5f3
Javad <mhossini3458@gmail.com>
2021-05-07 19:33:25 +0430

    add userController & Validation file

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -3,16 +3,17 @@ import { Request, Response } from 'express';
 import { User } from './../Models/userModel';
 
 const userPost = async (req: Request, res: Response) => {
+    console.log(req.secure+"  "+req.headers['x-forwarded-proto']);
     User.create(req.body)
     .then(user=>{
-        res.redirect('/?userCreated=true');
+        res.redirect('/login?userCreated=true');
         // res.status(200).json({
         //     message:"user created with blow data",
         //     data:user
         // }).redirect('/');
     })
     .catch(err=>{
-        res.redirect('/?userCreated=false');
+        res.redirect('/register?userCreated=false');
         // res.status(422).json({
         //     message:"falid",
         //     data:err
--- src/utils/Validation.ts
+++ src/utils/Validation.ts
@@ -26,6 +26,7 @@ const createUserValidator = () =>{
 const validate = async (req:Request,res:Response,next:any)=>{
     const errors = validationResult(req)
     if(!errors.isEmpty()){
+        next()
         return res.status(422).json({errors:errors.array()});
     }
     next();
